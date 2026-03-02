792e54f2d1a00a78dbeb682622e4dae3619cd3fa
Chitransh Patel <102661644+chitranshpatel@users.noreply.github.com>
2026-03-02 19:50:24 +1100

    Update basic_joins_solutions.sql

--- Basic_Joins/basic_joins_solutions.sql
+++ Basic_Joins/basic_joins_solutions.sql
@@ -13,3 +13,18 @@ JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
 ORDER BY g.grade DESC, name ASC, s.marks ASC;
 
 
+--2.Finding the Group-wise Minimum
+
+SELECT w.id, p.age, w.coins_needed, w.power
+FROM wands w
+JOIN wands_property p ON w.code = p.code
+WHERE p.is_evil = 0
+  AND w.coins_needed = (
+      SELECT MIN(w1.coins_needed)
+      FROM wands w1
+      JOIN wands_property p1 ON w1.code = p1.code
+      WHERE p1.is_evil = 0 
+        AND w1.power = w.power 
+        AND p1.age = p.age
+  )
+ORDER BY w.power DESC, p.age DESC;
