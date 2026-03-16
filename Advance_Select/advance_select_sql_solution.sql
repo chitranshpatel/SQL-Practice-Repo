8f7b3beb589152fa1bcb8c05e3bb90da08a30874
Chitransh Patel <102661644+chitranshpatel@users.noreply.github.com>
2026-03-16 17:14:38 +1100

    Update advance_select_sql_solution.sql

--- Advance_Select/advance_select_sql_solution.sql
+++ Advance_Select/advance_select_sql_solution.sql
@@ -10,3 +10,19 @@ FROM occupations
 Group by occupation
 order by COUNT(*), occupation;
 
+
+-- 2 Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation.
+
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor'    THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer'    THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor'     THEN Name END) AS Actor
+FROM (
+    SELECT Name, Occupation,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM OCCUPATIONS
+) AS temp
+GROUP BY rn;
+
+
