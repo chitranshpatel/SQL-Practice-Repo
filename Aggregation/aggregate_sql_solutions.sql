0ba4447fec14ece542154cfc7e7832761761b319
Chitransh Patel <102661644+chitranshpatel@users.noreply.github.com>
2026-03-23 17:22:04 +1100

    Add query for user post date range in 2021
    Add query to calculate days between first and last post for users in 2021.

--- Aggregation/aggregate_sql_solutions.sql
+++ Aggregation/aggregate_sql_solutions.sql
@@ -43,3 +43,14 @@ WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
 group by candidate_id) as count_skills
 where no_skill = 3
 order by candidate_id asc;
+
+--5.  write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021. 
+-- Output the user and number of the days between each user's first and last post.
+
+SELECT 
+	user_id, 
+    MAX(post_date::DATE) - MIN(post_date::DATE) AS days_between
+FROM posts
+WHERE DATE_PART('year', post_date::DATE) = 2021 
+GROUP BY user_id
+HAVING COUNT(post_id)>1;
